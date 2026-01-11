fdfe4e860dc4ca80562611a43223162bdbd95f31
rchtsang <rchtsang@ucdavis.edu>
2026-01-11 00:29:23 -0500

    move content section to base and remove logging from tabs.js

--- theme/editorial/static/js/tabs.js
+++ theme/editorial/static/js/tabs.js
@@ -1,13 +1,11 @@
 function show_tab_class(evt, tabcls, tabname) {
 	// get elements with class "tab-content <tabcls>"
-	console.log(evt, tabcls, tabname)
 	let elems = document.getElementsByClassName("tab-content " + tabcls);
 	for (let i = 0; i < elems.length; i++) {
 		elems[i].className = elems[i].className.replace(" active", "");
 		if (elems[i].className.includes(tabname)) {
 			elems[i].className += " active";
 		}
-		console.log(elems[i].className)
 	}
 
 	// get elements with class "tab-link <tabcls>"
